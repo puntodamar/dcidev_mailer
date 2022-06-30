38caa0a6b0f89e46aab6b9165346aa297ba96fdf
puntodamar <punto@privyid.tech>
2022-06-30 16:33:27 +0700

    Fix: cc not showing

--- lib/dcidev_mailer/mandrill.rb
+++ lib/dcidev_mailer/mandrill.rb
@@ -5,17 +5,17 @@ module DcidevMailer
         default from: ENV['DEFAULT_EMAIL_SENDER']
 
         class << self
-            def send_email(subject: '', html_body: '', to: nil, cc: nil, bcc: nil, from: nil, from_name: nil, attachments: nil, email_template_path: '', header_url: '', footer_url: '')
+            def send_email(subject: '', html_body: '', to: nil, cc: nil, bcc: nil, from: nil, from_name: nil, attachments: nil, email_template_path: '', header_url: '', footer_url: '', preserve_recipients: false)
                 ac = ActionController::Base.new
                 wording, images = DcidevMailer.format_image_from_html(html_body)
                 locals = { wording: wording, header: nil, footer: nil }
                 locals, images = DcidevMailer.format_header_footer(header_url: header_url, footer_url: footer_url, locals: locals, images: images) if header_url.present? && footer_url.present?
                 html_body = ac.render_to_string(template: email_template_path, locals: locals)
                 attachments = DcidevMailer.format_attachments(attachments) if attachments.present?
-                self.send_mail(subject, to, cc, bcc, html_body, attachments, images, from, from_name).deliver_now
+                self.send_mail(subject, to, cc, bcc, html_body, attachments, images, from, from_name, preserve_recipients).deliver_now
             end
 
-            def send_mail(subject, to, cc, bcc, html, attachments = nil, images = nil, from = nil, from_name = nil)
+            def send_mail(subject, to, cc, bcc, html, attachments = nil, images = nil, from = nil, from_name = nil, preserve_recipients)
                 mandrill_mail subject: subject,
                               from: from,
                               from_name: from_name,
@@ -29,7 +29,8 @@ module DcidevMailer
                               important: true,
                               inline_css: true,
                               attachments: attachments,
-                              images: images
+                              images: images,
+                              preserve_recipients: cc.present? || preserve_recipients
             end
         end
     end
--- lib/dcidev_mailer/mandrill_template.rb
+++ lib/dcidev_mailer/mandrill_template.rb
@@ -5,14 +5,14 @@ module DcidevMailer
         default from: ENV['DEFAULT_EMAIL_SENDER']
 
         class << self
-            def send_email(subject: '', to: nil, cc: nil, bcc: nil, from: nil, from_name: nil, attachments: nil, vars: nil, template_name: nil, images: nil)
+            def send_email(subject: '', to: nil, cc: nil, bcc: nil, from: nil, from_name: nil, attachments: nil, vars: nil, template_name: nil, images: nil, preserve_recipients: false)
                 raise DcidevMailer::Errors::InvalidTemplate unless template_name.present?
                 images = DcidevMailer.format_images(images) if images.present?
                 attachments = DcidevMailer.format_attachments(attachments) if attachments.present?
-                self.send_mail(subject, to, cc, bcc, attachments, images, from, from_name, template_name, vars).deliver_now
+                self.send_mail(subject, to, cc, bcc, attachments, images, from, from_name, template_name, vars, preserve_recipients).deliver_now
             end
 
-            def send_mail(subject, to, cc, bcc, attachments = nil, images = nil, from = nil, from_name = nil, template_name = nil, vars = nil)
+            def send_mail(subject, to, cc, bcc, attachments = nil, images = nil, from = nil, from_name = nil, template_name = nil, vars = nil, preserve_recipients)
                 mandrill_mail subject: subject,
                               from: from,
                               from_name: from_name,
@@ -24,7 +24,8 @@ module DcidevMailer
                               attachments: attachments,
                               images: images,
                               template_name: template_name,
-                              vars: vars
+                              vars: vars,
+                              preserve_recipients: cc.present? || preserve_recipients
             end
         end
     end
